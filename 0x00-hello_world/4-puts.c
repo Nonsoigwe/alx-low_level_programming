e93e2e375ce16df4d50ce8ed34e5f763a3b68eeb
Nonsoigwe <chinonsoigwe007@gmail.com>
2022-09-12 09:16:32 -0700

    omo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 
 #include <stdio.h>    
 
+/**
+ * main
+ *
+ * Return Always 0 (Success)
+ */
+
 int main(void)
 {    
 
-	puts("\"programming is like building a multilingual puzzle");    
+	puts("\"programming is like building a multilingual puzzlei");    
 
 	return 0;   
 
