c49d99d66bf289627cdd0625ad4e3ad6d83bd0ad
Nonsoigwe <chinonsoigwe007@gmail.com>
2022-09-12 04:26:57 -0700

    omo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,5 @@
 #include <studio.h>
 
-
-
-/**
- *
- * * main - print the string in the put function
- *
- * *
- *
- * *Description: using the main function
- *
- * * this program prints "programming is like building a mulitilingual puzzle"
- *
- * * Return: 0
- *
- * */
-
 int main(void)
 
 {
