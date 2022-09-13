f1fbb54317d36e7c8208fb11d5c7f2adb5132308
Nonsoigwe <chinonsoigwe007@gmail.com>
2022-09-12 21:07:53 -0700

    omo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,8 @@
  *this program prints with proper grammar, but the outcome is a piece of art,
  *Return: 0
  */
- int main(void)
- {
-	 printf("with proper grammar, but the outcome is a piece of art,");
-	 return (0);
- }
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
